81f23238484bfe40320a42464c153ae4cb72fea5
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-05-06 16:56:05 +0200

    Aggiunto model Articles (con factory, migration e seed)

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,6 +12,7 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+/*
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
@@ -20,5 +21,14 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+ */
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Models\Article::class, function (Faker\Generator $faker) {
+    return [
+        'code' => $faker->unique()->uuid,
+        'description' => $faker->sentence(4, true)
     ];
 });
---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        factory(App\Models\Article::class, 100)->create()->each(function ($article) {
+            $article->save();
+        });
+    }
+
+}
